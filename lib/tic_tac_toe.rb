38347c565330ed3920450247451a0e779b39bbff
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-23 19:16:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -183,6 +183,6 @@ end
 def play(board)
   turn(board) until over?(board)
   if won?(board)
-    puts "Congratulations winner #{won?(board)}!"
+    puts "Congratulations winner #{winner?(board)}!"
   end
 end
